9956903b2822f79978bfa2626792045662f58b5d
P. Büttiker <isnull@securityvision.ch>
2013-10-24 17:40:47 +0200

    asm adjustments

--- Source/EagleCompiler/postprocess/create_exe.sh
+++ Source/EagleCompiler/postprocess/create_exe.sh
@@ -4,13 +4,13 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]
 then
-  os=macho32
+  os=macho
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
 then
-  os=elf32
+  os=elf64
 fi
 #For 64-bit binary code, remove "--32"
 nasm $1 -o $1".o" -f $os 
 #For 64-bit executable remove "-m elf_i386"
-ld -s $1".o" -o $2 -m elf_i386 
+ld -s $1".o" -o $2 
 rm $1".o" 2>/dev/null
--- Source/EagleCompiler/postprocess/tmp.as
+++ Source/EagleCompiler/postprocess/tmp.as
@@ -1,17 +1,21 @@
-global _start
+global start
 
 section .text
-_start:
-mov edx,6
-mov ecx,myText
-mov ebx,1
-mov eax,4
+start:
+push dword 4
+push myText
+push 1
+mov eax, 0x4
+sub esp, 4  
 int 0x80
 
 ;systemcall exit(0)
 push dword 0
-mov eax,1
+mov eax, 0x1
+sub esp, 4  
 int 0x80
+ret
+
 
 section .data
 myText: db "foo", 0xa
