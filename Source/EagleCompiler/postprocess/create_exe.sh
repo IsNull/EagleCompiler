41b0ba8486fa865103ce5071d4c3572f21812583
samuel.vonstachelski <samuel.vonstachelski@students.fhwn.ch>
2013-10-24 16:51:01 +0000

    Changed assembler to nasm, script is now platform-indepentent

--- Source/EagleCompiler/postprocess/create_exe.sh
+++ Source/EagleCompiler/postprocess/create_exe.sh
@@ -1,9 +1,16 @@
 #$1 is the name of the intput file (assembler file)
 #s2 is the name of the output file (executable)
 
-#!/bin/sh
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]
+then
+  os=macho32
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+  os=elf32
+fi
 #For 64-bit binary code, remove "--32"
-as $1 -o $1".o" --32
+nasm $1 -o $1".o" -f $os 
 #For 64-bit executable remove "-m elf_i386"
-ld -s $1".o" -o $2 -m elf_i386
+ld -s $1".o" -o $2 -m elf_i386 
 rm $1".o" 2>/dev/null
