64620b93f4db3d26ab6c2a586065ee361a4d7f00
samuel.vonstachelski <samuel.vonstachelski@students.fhwn.ch>
2014-01-05 19:08:10 +0000

    Added some iml-sourcefiles, should be appended in final document

--- Source/EagleCompiler/postprocess/template.asm
+++ Source/EagleCompiler/postprocess/template.asm
@@ -1,18 +1,41 @@
-section     .text
 global      _start                              ;must be declared for linker (ld)
+
+;Some constants
+sys_exit equ 1
+sys_read equ 3
+sys_write equ 4
+stdin equ 0
+stdout equ 1
+stderr equ 2
+LF equ 10
+
+section     .text
 
 _start:                                         ;tell linker entry point
 
-    mov     edx,len                             ;message length
+    mov     edx,0xFF                             ;message length
     mov     ecx,msg                             ;message to write
-    mov     ebx,1                               ;file descriptor (stdout)
-    mov     eax,4                               ;system call number (sys_write)
+    mov     ebx,stdout                          ;file descriptor (stdout)
+    mov     eax,sys_write                       ;system call number (sys_write)
     int     0x80                                ;call kernel
 
-    mov     eax,1                               ;system call number (sys_exit)
+    mov     eax,sys_exit                        ;system call number (sys_exit)
     int     0x80                                ;call kernel
+;------------------------------------------------------------------------------
+GetStrlen:
+	push    ebx
+	xor     ecx, ecx
+	not     ecx
+	xor     eax, eax
+	cld
+	repne   scasb
+	mov     byte [edi - 1], 10
+	not     ecx
+	pop     ebx
+	lea     edx, [ecx - 1]
+ret
+
 
 section     .data
-
 msg     db  'Hello, world!',0xa                 ;our dear string
-len     equ $ - msg                             ;length of our dear string
+times 0x256-($-msg) db 0
