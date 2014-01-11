83fddf18ab09185e02a1de9ecf11747cf50c6bf9
IsNull <isnull@securityvision.ch>
2014-01-11 16:32:15 +0100

    generate code after AST creatrion

--- Source/EagleCompiler/main.cpp
+++ Source/EagleCompiler/main.cpp
@@ -23,7 +23,7 @@ using namespace std;
 string sourceCode;
 string serializedParseTable;
 
-void scan() {
+void compile() {
 
     // tokenize it:
     cout << "\nListing Source code:\n\n"<< sourceCode << "\n\n";
@@ -47,8 +47,10 @@ void scan() {
         
         cout << "\n\nGenerating AST:\n";
         
-        ASTGenerator astGen;
-        AST::CodeProgram* p = astGen.generate(syntaxtree);
+        ASTGenerator astGen(syntaxtree);
+        AST::CodeProgram* p = astGen.generate();
+        
+        cout << "Compiled ASM:\n" << p->code() << endl;
         
     }catch(GrammarException* ex){
         cout << "\n\nGrammarException: " << ex->what() << "\n";
@@ -210,7 +212,7 @@ int main(int argc, char* argv[])
     }
     
     
-	scan();
+	compile();
 #endif
 #ifdef SAM
 	testSam();
