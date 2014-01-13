cac7eeb8d1f22dd61533f9403ec8dfd636519534
samuel.vonstachelski <samuel.vonstachelski@students.fhwn.ch>
2014-01-13 20:34:08 +0000

    Changed build-process, copy now create_bin.sh to EagleCompiler executable directory, to directly compile and generate binary file

--- Source/EagleCompiler/postprocess/create_bin.sh
+++ Source/EagleCompiler/postprocess/create_bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-#$1 is the name of the intput file (iml file)
+#$1 is the name of the intput file (asm file)
 #s2 is the name of the output file (executable)
 
 #echo "Eaglecompiler > /tmp/out.asm... "
@@ -19,10 +19,9 @@ then
   os=elf
 fi
 
-echo "nasm > /tmp/out.o... "
-nasm $1 -o /tmp/out.o -f $os
+echo "Generate objectcode from assembler-file "$1"... "
+nasm $1 -o /tmp/out.o -f $os -g
+echo "Linking executable "$2"... "
+gcc -O0 -g -m32 /tmp/out.o -o $2
+rm /tmp/out.o
 echo "Done"
-echo "gcc > "$2"... "
-gcc -m32 /tmp/out.o -o $2
-echo "Done"
-echo "All done, Executable is "$2
